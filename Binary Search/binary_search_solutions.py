bbf9ab5a0f47540e672ccdadaf0dfc7e0f9b6386
Stanley Wang <wangstanley910@gmail.com>
2024-07-30 12:00:00 -0400

    875. Bananas!

--- Binary Search/binary_search_solutions.py
+++ Binary Search/binary_search_solutions.py
@@ -34,4 +34,20 @@ class Solution:
                         return True 
                 break
         return False
+
+    # 875. Koko Eating Bananas / Medium / 47 minutes / (https://leetcode.com/problems/koko-eating-bananas/) -- Review
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+            if hours <= h:
+               res = min(res, k)
+               r = k - 1
+            else:
+                l = k + 1
+        return res
     
