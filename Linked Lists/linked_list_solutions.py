16af9ef52eaed77bce2f68f6e45567065b0c4efe
Stanley Wang <wangstanley910@gmail.com>
2024-08-12 14:56:01 -0400

    287

--- Linked Lists/linked_list_solutions.py
+++ Linked Lists/linked_list_solutions.py
@@ -139,4 +139,19 @@ class LinkedList:
                 return True
         return False
 
-        
+    # 287. Find the Duplicate Number / Medium / 31 minutes / (https://leetcode.com/problems/find-the-duplicate-number/)
+    def findDuplicate(self, nums: List[int]) -> int:# LL Cycle implementation
+        # O(n) M O(1) Floyd's Cycle Finding Algorithm
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow] # the 'node' the value points at
+            fast = nums[nums[fast]] 
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow # The number 
