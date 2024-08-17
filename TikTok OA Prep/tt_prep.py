47bcb06acc44dad77d258f35269c8e8b7ca2f396
Stanley Wang <wangstanley910@gmail.com>
2024-08-17 14:23:21 -0400

    back131

--- TikTok OA Prep/tt_prep.py
+++ TikTok OA Prep/tt_prep.py
@@ -38,10 +38,33 @@ class TikTokPrep:
         return merge
 
 
+    # 131. Palindrom Partition -- Yeah I don't understand jackshit here
+    def partition(self, s: str) -> List[List[str]]:
+        res = [] # stores all partitions
+        part = [] # stores current partitions
 
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j + 1)
+                    part.pop()
+        dfs(0)
+        return res
 
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
 
+        
 
+     
 
 
 
