1c9cbcd7bbdcd2ad51d0b6d63826df1350603b7f
Stanley Wang <wangstanley910@gmail.com>
2024-08-23 17:09:18 -0400

    100

---
+++ OAs/OA 8-22/100.same-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # One null, the other not!
+        if not p and not q:
+            return True
+        elif not p or not q or p.val != q.val: 
+            return False
+
+        # Return the AND of both bool results to determine Sameness
+        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right))
+        
+
+
+      
+
+# @lc code=end
+
---
+++ OAs/OA 8-22/572.subtree-of-another-tree.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=572 lang=python3
+#
+# [572] Subtree of Another Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+# @lc code=end
+
