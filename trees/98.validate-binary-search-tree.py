611b78da0001058d1fff82ea99e25723786aeee4
Stanley <wangstanley910@gmail.com>
2024-09-05 17:06:48 -0400

    review

--- trees/98.validate-binary-search-tree.py
+++ trees/98.validate-binary-search-tree.py
@@ -1,10 +1,12 @@
 def isValidBST(self, root: Optional[TreeNode]) -> bool:
-    def valid(node, left, right):
+    # Need to confirm at each point is less / greater than left, right
+    def dfs(node, left, right):
         if not node:
             return True
+
         if not (left < node.val < right):
             return False
-        return valid(node.left, left, node.val) and valid(node.right, node.val, right)
 
-    left, right = float("-inf"), float("inf")
-    return valid(root, left, right)
+        return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
+
+    return dfs(root, float("-inf"), float("inf"))
