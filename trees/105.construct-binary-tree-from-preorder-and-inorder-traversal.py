1ec3f0cc5525e8c31cec0a9ae26fa41f8ee3a53d
Stanley <wangstanley910@gmail.com>
2024-09-08 19:05:14 -0400

    review

--- trees/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ trees/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,23 +1,30 @@
 def buildTree(self, preorder, inorder):
-    """
-    :type preorder: List[int]
-    :type inorder: List[int]
-    :rtype: TreeNode
-    """
-    if len(preorder) == 0:
+    #     """
+    #     :type preorder: List[int]
+    #     :type inorder: List[int]
+    #     :rtype: TreeNode
+    #     """
+    #     if len(preorder) == 0:
+    #         return None
+    #     value = preorder.pop(0)
+    #     l, r = self.split_array(inorder, value)
+    #     print(l, r)
+    #
+    #     l_tree = None if not l else self.buildTree(preorder, l)
+    #     r_tree = None if not r else self.buildTree(preorder, r)
+    #
+    #     return TreeNode(value, l_tree, r_tree)
+    #
+    #
+    # def split_array(self, arr, value):
+    #     i = 0
+    #     while arr[i] != value:
+    #         i += 1
+    #     return arr[0:i], arr[i + 1 : len(arr)]
+    if not preorder or not inorder:
         return None
-    value = preorder.pop(0)
-    l, r = self.split_array(inorder, value)
-    print(l, r)
-
-    l_tree = None if not l else self.buildTree(preorder, l)
-    r_tree = None if not r else self.buildTree(preorder, r)
-
-    return TreeNode(value, l_tree, r_tree)
-
-
-def split_array(self, arr, value):
-    i = 0
-    while arr[i] != value:
-        i += 1
-    return arr[0:i], arr[i + 1 : len(arr)]
+    root = TreeNode(preorder[0])
+    m = inorder.index(preorder[0])
+    root.left = self.buildTree(preorder[1 : m + 1], indorder[:m])
+    root.right = self.buildTree(preorder[m + 1 :], indorder[m + 1 :])
+    return root
