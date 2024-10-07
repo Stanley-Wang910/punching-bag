d4793d774e09b16666ac26afaad3092b1138ff28
twinfantasyfan <wangstanley910@gmail.com>
2024-10-06 20:57:51 -0400

    53

---
+++ greedy/53.maximum-subarray.py
@@ -0,0 +1,10 @@
+def maxSubArray(self, nums: List[int]) -> int:
+    max_sub = nums[0]
+    cur_sum = 0
+
+    for n in nums:
+        if cur_sum < 0:
+            cur_sum = 0
+        cur_sum += n
+        max_sub = max(max_sub, cur_sum)
+    return max_sub
